94033f6b517376664b1b6174df2c308b91fea1b4
Sithar Norbu <cthanobu@gmail.com>
2011-04-29 11:44:25 +0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shovell::Application.config.secret_token = 'a95dc9eb3ca07a01766d2f961dfd884e94bf23446ebd646605725bca2dcd3caad40845a2d45f0ddb21f6df3b06e30762337e80fbcfdda85bba00de498629d423'
